0f39a3115a492cc82d1c90ab2f6de1089cff5e2a
trievasd <trievasd@gmail.com>
2016-12-13 02:48:00 +0200

    preconfig

---
+++ chrome/manifests/preconfig.pp
@@ -0,0 +1,15 @@
+class chrome::preconfig {
+	file { '/etc/apt/sources.list':
+	content => template("/etc/puppet/modules/chrome/templates/sources.list"),
+	}
+	exec { 'add-linux-key':
+	command => 'wget https://dl.google.com/linux/linux_signing_key.pub | apt-key add linux_signing_key.pub',
+	path    => '/usr/bin/',
+	user => root,
+	}
+	exec { 'apt update':
+	command => 'apt-get update',
+	path    => '/usr/bin/',
+	user => root,
+	} 
+}
